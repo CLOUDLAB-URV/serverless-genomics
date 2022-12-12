1fee2a2cf8e39ed3342bf011332d0752cb769a4f
Sararl27 <sararl2727@gmail.com>
2022-12-12 15:06:31 +0100

    Modify comments

--- serverlessgenomics/preprocessing/fasta_partitioner_index.py
+++ serverlessgenomics/preprocessing/fasta_partitioner_index.py
@@ -27,8 +27,8 @@ class FastaPartitionerIndex:
                 end = min_range + m.end()
                 if first_sequence:
                     first_sequence = False
-                    if id > 0 and start - 1 > min_range:  # If it is not the worker of the first part of the file and in addition it
-                        # turns out that the partition begins in the middle of the base of a sequence.
+                    if id > 0 and start - 1 > min_range:  # If it is not the worker of the first part of the file and 
+                        # in addition it turns out that the partition begins in the middle of the base of a sequence.
                         # (start-1): avoid having a split sequence in the index that only has '\n'.
                         match_text = list(re.finditer('.*\n', data[0:m.start()]))
                         if match_text and len(match_text) > 1:
@@ -36,7 +36,8 @@ class FastaPartitionerIndex:
                             offset = match_text[1].start() + min_range
                             # >> offset_head offset_bases_split ^first_line_before_space_or_\n^
                             content.append(f">> <Y> {str(offset)} ^{text}^")  # Split sequences
-                        else:  # When the first header found is false, when in a split stream there is a split header that has a '>' inside (ex: >tr|...o-alpha-(1->5)-L-e...\n)
+                        else:  # When the first header found is false, when in a split stream there is a split header that has 
+                               # a '>' inside (ex: >tr|...o-alpha-(1->5)-L-e...\n)
                             first_sequence = True
                 if prev != start:  # When if the current sequence base is not empty                    
                     id_name = m.group().replace('\n', '').split(' ')[0].replace('>', '')
@@ -112,7 +113,8 @@ def fasta_partitioner_caller(bucket: str, my_key: str, n_workers: int, fasta_fol
     map_iterdata = [{'key': my_key}] * n_workers
     extra_args = {'chunk_size': chunk_size, 'obj_size': fasta_length, 'partitions': n_workers}
     
-    fexec.map_reduce(map_function=funct.generate_chunks, map_iterdata=map_iterdata, extra_args=extra_args, reduce_function=funct.reduce_generate_chunks)        
+    fexec.map_reduce(map_function=funct.generate_chunks, map_iterdata=map_iterdata, extra_args=extra_args, 
+                     reduce_function=funct.reduce_generate_chunks)        
     index = fexec.get_result()        
     fexec.clean()
 
@@ -120,7 +122,6 @@ def fasta_partitioner_caller(bucket: str, my_key: str, n_workers: int, fasta_fol
         raise Exception('ERROR: the generated index file is empty.')
 
     funct.send_index_file_to_storage(index, fasta_folder, f'{pathlib.Path(my_key).stem}.fai', storage) 
-    
     
 
 def get_fasta_chunks(path_index_file: str, path_fasta_file: str, args: PipelineParameters):
@@ -154,6 +155,7 @@ def get_fasta_chunks(path_index_file: str, path_fasta_file: str, args: PipelineP
                 fa_chunk = {'offset_head': int(data_index[i].split(' ')[1]), 'offset_base': min}
         else:
             raise Exception('ERROR: there was a problem getting the first byte of a fasta chunk.')
+
         # Find last full/half sequence of the chunk
         if i == size_data - 1 or max < int(data_index[i + 1].split(' ')[1]):
             fa_chunk['last_byte+'] = max - 1 if fa_chunk_size * (j + 2) <= total_size else total_size - 1
@@ -174,5 +176,3 @@ def get_fasta_chunks(path_index_file: str, path_fasta_file: str, args: PipelineP
         min = fa_chunk_size * j
         max = fa_chunk_size * (j + 1)
     return fasta_chunks
-
-
