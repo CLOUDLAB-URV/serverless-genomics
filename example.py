b7033f16d75ae972f62a44fd09918055fd70cda0
Arnau Gabriel Atienza <arnau12321@gmail.com>
2022-12-21 11:09:54 +0100

    Full Execution Example

--- example.py
+++ example.py
@@ -5,9 +5,9 @@ from serverlessgenomics import VariantCallingPipeline
 if __name__ == '__main__':
     pipeline = VariantCallingPipeline(
         fasta_path='s3://agabriel-data/fasta/TriTrypDB-9.0_TbruceiTREU927_Genome_MbChr.fasta',
-        fasta_chunks=10,
+        fasta_chunks=5,
         fastq_path='s3://agabriel-data/fastq/SRR935389.fastq.gz',
-        fastq_chunks=70,
+        fastq_chunks=20,
         log_level=logging.DEBUG,
         storage_bucket='agabriel-data',
         override_id='616d81c4-192c-4775-92f0-41bcf3653d0a'
